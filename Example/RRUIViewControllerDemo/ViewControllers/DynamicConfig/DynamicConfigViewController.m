52db7f983b5b3c0780b95295a44d69b2fbac5ff7
zxllf23 <zxllf23@163.com>
2022-09-14 17:46:03 +0800

    for ios16

--- Example/RRUIViewControllerDemo/ViewControllers/DynamicConfig/DynamicConfigViewController.m
+++ Example/RRUIViewControllerDemo/ViewControllers/DynamicConfig/DynamicConfigViewController.m
@@ -6,8 +6,8 @@
 //  Copyright © 2018年 深圳市两步路信息技术有限公司. All rights reserved.
 //
 
+@import RRViewControllerExtension;
 #import "DynamicConfigViewController.h"
-#import "RRViewControllerExtension.h"
 #import "DEMO_ImageNaviBarViewController.h"
 #import "DEMO_HiddenNaviBarViewController.h"
 #import "DEMO_TransparentNaviBarViewController.h"
